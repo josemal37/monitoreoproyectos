9664e4f2aab3588c5b6c5e15d8196d13fae09911
josemal37 <josemal_37@yahoo.es>
2017-06-02 12:01:11 -0400

    añadida funcion para limpiar nombres de archivos

--- application/libraries/Archivo.php
+++ application/libraries/Archivo.php
@@ -29,7 +29,7 @@ class Archivo {
 
 		if ($archivos) {
 			if ($path) {
-				$path = sanitize_spanish_filename($path);
+				$path = $this->sanitize_spanish_filename($path);
 				$path = str_replace(" ", "_", $path);
 				$path = self::PATH_BASE_ARCHIVO . $path;
 			} else {
@@ -50,7 +50,7 @@ class Archivo {
 
 	private function guardar_archivos($path, $archivos) {
 		$guardado = FALSE;
-		
+
 		$this->ci->upload->initialize(array(
 			"upload_path" => $path,
 			"allowed_types" => self::EXTENSIONES_VALIDAS
@@ -58,7 +58,7 @@ class Archivo {
 
 		if ($this->ci->upload->do_multi_upload($archivos)) {
 			$guardado = $this->ci->upload->get_multi_upload_data();
-			
+
 			if ($guardado) {
 				$i = 0;
 				foreach ($guardado as $archivo) {
@@ -69,7 +69,7 @@ class Archivo {
 		} else {
 			$this->ci->session->set_flashdata("error_archivo", implode(" ", $this->ci->upload->error_msg));
 		}
-		
+
 		return $guardado;
 	}
 
@@ -85,6 +85,29 @@ class Archivo {
 		}
 
 		return $creado;
+	}
+
+	private function sanitize_spanish_filename($str, $relative_path = FALSE) {
+
+		$str = str_replace(array('á', 'à', 'â', 'ã', 'ª', 'ä'), "a", $str);
+		$str = str_replace(array('Á', 'À', 'Â', 'Ã', 'Ä'), "A", $str);
+		$str = str_replace(array('Í', 'Ì', 'Î', 'Ï'), "I", $str);
+		$str = str_replace(array('í', 'ì', 'î', 'ï'), "i", $str);
+		$str = str_replace(array('é', 'è', 'ê', 'ë'), "e", $str);
+		$str = str_replace(array('É', 'È', 'Ê', 'Ë'), "E", $str);
+		$str = str_replace(array('ó', 'ò', 'ô', 'õ', 'ö', 'º'), "o", $str);
+		$str = str_replace(array('Ó', 'Ò', 'Ô', 'Õ', 'Ö'), "O", $str);
+		$str = str_replace(array('ú', 'ù', 'û', 'ü'), "u", $str);
+		$str = str_replace(array('Ú', 'Ù', 'Û', 'Ü'), "U", $str);
+		$str = str_replace(array('[', '^', '´', '`', '¨', '~', ']', ',', '+', '=', '&'), "", $str);
+		$str = str_replace("ç", "c", $str);
+		$str = str_replace("Ç", "C", $str);
+		$str = str_replace("ñ", "n", $str);
+		$str = str_replace("Ñ", "N", $str);
+		$str = str_replace("Ý", "Y", $str);
+		$str = str_replace("ý", "y", $str);
+
+		return $str;
 	}
 
 }
--- application/libraries/MY_Upload.php
+++ application/libraries/MY_Upload.php
@@ -379,7 +379,7 @@
 						
 						//Sanitize the file name for security.
 						$this->file_name = sanitize_filename($this->file_name);
-						$this->file_name = sanitize_spanish_filename($this->file_name);
+						$this->file_name = $this->sanitize_spanish_filename($this->file_name);
 						
 						//Truncate the file name if it's too long
 						if($this->max_filename > 0){
@@ -442,5 +442,28 @@
 					
 					//Return all file upload data.
 					return TRUE;
+			}
+			
+			private function sanitize_spanish_filename($str, $relative_path = FALSE) {
+
+				$str = str_replace(array('á', 'à', 'â', 'ã', 'ª', 'ä'), "a", $str);
+				$str = str_replace(array('Á', 'À', 'Â', 'Ã', 'Ä'), "A", $str);
+				$str = str_replace(array('Í', 'Ì', 'Î', 'Ï'), "I", $str);
+				$str = str_replace(array('í', 'ì', 'î', 'ï'), "i", $str);
+				$str = str_replace(array('é', 'è', 'ê', 'ë'), "e", $str);
+				$str = str_replace(array('É', 'È', 'Ê', 'Ë'), "E", $str);
+				$str = str_replace(array('ó', 'ò', 'ô', 'õ', 'ö', 'º'), "o", $str);
+				$str = str_replace(array('Ó', 'Ò', 'Ô', 'Õ', 'Ö'), "O", $str);
+				$str = str_replace(array('ú', 'ù', 'û', 'ü'), "u", $str);
+				$str = str_replace(array('Ú', 'Ù', 'Û', 'Ü'), "U", $str);
+				$str = str_replace(array('[', '^', '´', '`', '¨', '~', ']', ',', '+', '=', '&'), "", $str);
+				$str = str_replace("ç", "c", $str);
+				$str = str_replace("Ç", "C", $str);
+				$str = str_replace("ñ", "n", $str);
+				$str = str_replace("Ñ", "N", $str);
+				$str = str_replace("Ý", "Y", $str);
+				$str = str_replace("ý", "y", $str);
+
+				return $str;
 			}
 		}
