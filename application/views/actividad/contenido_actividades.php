4e02389b55213d5c0a9b3d28d789c9e0bf922d0f
josemal37 <josemal_37@yahoo.es>
2017-06-17 23:59:21 -0400

    mejorado vista de actividades

--- application/views/actividad/contenido_actividades.php
+++ application/views/actividad/contenido_actividades.php
@@ -40,14 +40,6 @@
 
 						<p><label>Fecha de fin:</label> <?= $actividad->fecha_fin ?></p>
 
-						<?php if (isset($actividad->id_meta_actividad)): ?>
-
-							<p><label>Meta:</label> <?= $actividad->cantidad ?> <?= $actividad->unidad ?></p>
-
-						<?php endif; ?>
-
-						<p><label>Avance:</label> <?= $actividad->avance_acumulado + 0 ?> <?= $actividad->unidad ?></p>
-
 						<?php if ($actividad->finalizada): ?>
 
 							<p><label>Estado:</label> Cerrado</p>
@@ -55,6 +47,44 @@
 						<?php else: ?>
 
 							<p><label>Estado:</label> Abierto</p>
+
+						<?php endif; ?>
+
+						<?php if ($this->uri->segment(2) == "editar_actividades"): ?>
+
+							<?php if (isset($actividad->id_meta_actividad)): ?>
+
+								<p><label>Meta:</label> <?= $actividad->cantidad ?> <?= $actividad->unidad ?></p>
+
+							<?php endif; ?>
+
+							<p><label>Avance:</label> <?= $actividad->avance_acumulado + 0 ?> <?= $actividad->unidad ?></p>
+
+						<?php endif; ?>
+
+						<?php if ($this->uri->segment(2) == "ver_actividades"): ?>
+
+							<?php if (isset($actividad->id_meta_actividad)): ?>
+
+								<div class="progress">
+
+									<?php
+									$porcentaje_avance = ($actividad->avance_acumulado / $actividad->cantidad) * 100;
+
+									if ($porcentaje_avance > 100) {
+										$porcentaje_avance = 100;
+									}
+									?>
+
+									<div class="progress-bar" role="progressbar" aria-valuenow="<?= $actividad->avance_acumulado + 0 ?>" aria-valuemin="0" aria-valuemax="<?= $actividad->cantidad + 0 ?>" style="width: <?= $porcentaje_avance ?>%; min-width: 10em;">
+
+										<p><?= $actividad->avance_acumulado + 0 ?> / <?= $actividad->cantidad + 0 ?> <?= $actividad->unidad ?></p>
+
+									</div>
+
+								</div>
+
+							<?php endif; ?>
 
 						<?php endif; ?>
 
@@ -91,3 +121,17 @@
 	</div>
 
 </div>
+
+<script type="text/javascript">
+    $(".progress-bar").each(function () {
+        var $this = $(this);
+        var porcentaje = parseFloat($this[0].style.width);
+        if (porcentaje < 33.33) {
+            $this.addClass("progress-bar-danger");
+        } else if (porcentaje >= 33.33 && porcentaje < 66.67) {
+            $this.addClass("progress-bar-warning");
+        } else {
+            $this.addClass("progress-bar-success");
+        }
+    });
+</script>
